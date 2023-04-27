0ef53d185aa2268c159854b543efa658d63cfb5c
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-04-27 12:50:35 +0200

    Datos de chapín
    Datos de chapín

--- CANSAT_2023/Calibracion.R
+++ CANSAT_2023/Calibracion.R
@@ -1,7 +1,7 @@
 #Apartado 1
-datos= read.csv("RESUMEN.csv", header= TRUE,dec=",")
-# datos=datos[1:4,]
-datos=head(datos,5)
+datos= read.csv("RESUMEN2.csv", header= TRUE,dec=",")
+datos=datos[-c(7,8),]
+# datos=head(datos,5)
 
 # Apartado 2
 # plot(datos$D, datos$P1, type = "l", main="Antena 1 - Brida blanca")
@@ -14,7 +14,7 @@ datos=head(datos,5)
 
 for (i in 2:4) {
 
-x = 1/ (datos$D**3)
+x = 1/ (datos$D)
 y = datos[,i]
 
 l1=lm(y~x)
@@ -24,10 +24,10 @@ po = l1$coefficients[1]
 b = l1$coefficients[2]
 R2 = summary(l1)$r.squared
 
-f1=function(x) {po+b/(x**3)}
+f1=function(x) {po+b/(x)}
 
 # Apartado 3
 plot(datos$D, y, type = "l", main=paste0("Antena ",i-1))
 lines(datos$D, f1(datos$D), type = "l",col="red")
-print(paste0("P[",i-1,"]=",po,b,"/(d^3)  con R2=",R2))
+print(paste0("P[",i-1,"]=",po,b,"/D  con R2=",R2))
 }
